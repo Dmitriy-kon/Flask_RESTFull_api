6e9592f3faa9f19b482bca7995d405b69815a097
Konovalov Dmitriy <dkonowalow@gmail.com>
2023-03-09 09:33:35 +0600

    Создание моделÐей

---
+++ core/models/model.py
@@ -0,0 +1,43 @@
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import fields, Schema
+
+db = SQLAlchemy()
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.String(100))
+    trailer = db.Column(db.String(100))
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+
+    genre = db.relationship("Genre", backref="movies")
+    director = db.relationship("Director", backref="movies")
+
+
+class Genre(db.Model):
+    __tablename__ = 'genre'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+
+class Director(db.Model):
+    __tablename__ = 'director'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+
+class MovieSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    rating = fields.Float()
+    genre_id = fields.Int()
+    director_id = fields.Int()
+
+    genre = db.relationship("Genre", backref="movies")
+    director = db.relationship("Director", backref="movies")
